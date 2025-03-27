7853e2f06a7acea6222b99ac85a703c5366c0764
douglas <douglas.arau@hotmail.com>
2025-03-27 13:02:14 -0300

    Comceçando primeira section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let currentIndex = 0; // Índice da imagem atual
-const images = document.querySelectorAll('.image-container img'); // Seleciona todas as imagens
-const totalImages = images.length; // Total de imagens
+const images = document.querySelectorAll('.image-container img');
+const totalImages = images.length;
 
 // Função para trocar a imagem
 function changeImage() {
@@ -15,4 +15,14 @@ function changeImage() {
 }
 
 // Troca a imagem a cada 3 segundos (3000ms)
-setInterval(changeImage, 6000);
+setInterval(changeImage, 3000);
+
+window.onscroll = function() { toggleNavbarClass() };
+
+    function toggleNavbarClass() {
+        if (window.scrollY > 50) {  // Quando o usuário rolar 50px ou mais
+            document.querySelector('.navbar').classList.add('scrolled');
+        } else {
+            document.querySelector('.navbar').classList.remove('scrolled');
+        }
+    }
